6aa971658ec35fed8ee220863b3c625998288c4a
do-park <doheepa@kakao.com>
2024-03-17 12:06:25 +0900

    feature: 자바코드 구성 정보 사용
    Factory Method
    @Configuration가 애플리케이션 컨텍스트에 가장 먼저 등록 (BeanFactoryMethod를 가지는 것 이상으로 전체 애플리케이션을 구성하는데 필요한 중요한 정보들을 많이 넣을 수 있다.)

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,13 +3,25 @@ package com.example.demo;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+@Configuration
 public class DemoApplication {
+    @Bean
+    public HelloController helloController(HelloService helloService) {
+        return new HelloController(helloService);
+    }
+
+    @Bean
+    public HelloService helloService() {
+        return new SimpleHelloService();
+    }
 
     public static void main(String[] args) {
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
             @Override
             protected void onRefresh() {
                 super.onRefresh();
@@ -24,8 +36,7 @@ public class DemoApplication {
                 webServer.start();
             }
         };
-        applicationContext.registerBean(HelloController.class);
-        applicationContext.registerBean(SimpleHelloService.class);
+        applicationContext.register(DemoApplication.class);
         applicationContext.refresh(); // 서블릿 컨테이너 초기화 작업
     }
 

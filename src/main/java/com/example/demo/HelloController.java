bd7630b9668e7eb2d413dca81b262441659a695c
do-park <doheepa@kakao.com>
2024-03-17 11:31:51 +0900

    feature: 애노테이션 매핑 정보 사용

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,7 +1,14 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.Objects;
 
+@Controller
+@RequestMapping
 public class HelloController {
     private final HelloService helloService;
 
@@ -9,6 +16,8 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    @GetMapping("/hello")
+    @ResponseBody
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }

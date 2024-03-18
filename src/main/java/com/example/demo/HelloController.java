12530770f3aea6fb290da62da57e5047e0755e34
do-park <doheepa@kakao.com>
2024-03-18 21:09:10 +0900

    feature: Bean의 생명주기 메소드
    생성자를 통해 애플리케이션 컨텍스트를 주입받는 방법이 더 낫다
    -> 생성자에서 저장이 되니까 final로 지정할 수도 있다

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,30 +1,25 @@
 package com.example.demo;
 
-import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
 @RestController
-public class HelloController implements ApplicationContextAware {
+public class HelloController {
     private final HelloService helloService;
-    private ApplicationContext applicationContext;
+    private final ApplicationContext applicationContext;
 
-    public HelloController(HelloService helloService) {
+    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
         this.helloService = helloService;
+        this.applicationContext = applicationContext;
+
+        System.out.println(applicationContext);
     }
 
     @GetMapping("/hello")
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
-    }
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        System.out.println(applicationContext);
-        this.applicationContext = applicationContext;
     }
 }

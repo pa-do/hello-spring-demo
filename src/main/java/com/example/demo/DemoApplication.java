649010ff1775d5aa5d3fd1ec3e78f9710502af4a
do-park <doheepa@kakao.com>
2024-03-18 21:18:29 +0900

    feature: SpringBootApplication

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,10 @@
 package com.example.demo;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
@@ -23,25 +21,9 @@ public class DemoApplication {
     }
 
     public static void main(String[] args) {
-        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-            @Override
-            protected void onRefresh() {
-                super.onRefresh();
-
-                ServletWebServerFactory serverFactory =this.getBean(ServletWebServerFactory.class);
-                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-                // dispatcherServlet.setApplicationContext(this); 해당 코드가 없어도 스프링 컨테이너가 주입한다
-
-                WebServer webServer = serverFactory.getWebServer(servletContext -> {
-
-                    servletContext.addServlet("dispatcherServlet", dispatcherServlet)
-                            .addMapping("/*");
-                });
-                webServer.start();
-            }
-        };
-        applicationContext.register(DemoApplication.class);
-        applicationContext.refresh(); // 서블릿 컨테이너 초기화 작업
+        MySpringApplication.run(DemoApplication.class, args);
     }
+
+
 
 }

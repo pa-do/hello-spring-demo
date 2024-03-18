152de4b5b89f341912890d41a322cf235d7befb4
do-park <doheepa@kakao.com>
2024-03-18 20:30:05 +0900

    feature: @Component 스캔
    애노테이션을 메타 애노테이션으로 가지고 있는 애노테이션을 붙여도 컴포넌트라는 애노테이션이 붙은 것과 동일한 효과
    
    메타 애노테이션: 애노테이션 위에 붙은 애노테이션

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,7 +3,6 @@ package com.example.demo;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,16 +1,11 @@
 package com.example.demo;
 
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
-@Controller
-@RequestMapping
-@Component
+@RestController
 public class HelloController {
     private final HelloService helloService;
 
@@ -19,7 +14,6 @@ public class HelloController {
     }
 
     @GetMapping("/hello")
-    @ResponseBody
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }

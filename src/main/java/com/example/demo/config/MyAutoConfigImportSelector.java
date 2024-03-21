981a538681dae958a4f0819bfeafe865cfa232d6
do-park <doheepa@kakao.com>
2024-03-21 21:25:45 +0900

    feature: 자동 구성 정보 파일 분리-2

--- src/main/java/com/example/demo/config/MyAutoConfigImportSelector.java
+++ src/main/java/com/example/demo/config/MyAutoConfigImportSelector.java
@@ -4,7 +4,8 @@ import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
-import java.util.stream.StreamSupport;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
     private final ClassLoader classLoader;
@@ -15,7 +16,12 @@ public class MyAutoConfigImportSelector implements DeferredImportSelector {
 
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        Iterable<String> candidates = ImportCandidates.load(MyAutoConfiguration.class, classLoader);
-        return StreamSupport.stream(candidates.spliterator(), false).toArray(String[]::new);
+        List<String> autoConfigs = new ArrayList<>();
+
+        for (String candidate: ImportCandidates.load(MyAutoConfiguration.class, classLoader)) {
+            autoConfigs.add(candidate);
+        }
+
+        return autoConfigs.toArray(new String[0]);
     }
 }

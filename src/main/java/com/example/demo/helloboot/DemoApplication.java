15c11d67d06e238a4d783e0caeec2558eed4de1a
do-park <doheepa@kakao.com>
2024-03-23 20:32:01 +0900

    feature: 자동 구성에 Environment 프로퍼티 적용

--- src/main/java/com/example/demo/helloboot/DemoApplication.java
+++ src/main/java/com/example/demo/helloboot/DemoApplication.java
@@ -1,22 +1,10 @@
 package com.example.demo.helloboot;
 
 import com.example.demo.config.MySpringBootApplication;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 
 @MySpringBootApplication
 public class DemoApplication {
-
-    @Bean
-    ApplicationRunner applicationRunner(Environment env) {
-        return args -> {
-            String name = env.getProperty("my.name");
-            System.out.println("my.name: " + name);
-        };
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
--- src/main/java/com/example/demo/helloboot/WebServerConfiguration.java
+++ src/main/java/com/example/demo/helloboot/WebServerConfiguration.java
@@ -4,12 +4,13 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration(proxyBeanMethods = false)
 public class WebServerConfiguration {
-    @Bean ServletWebServerFactory customerWebServerFactory() {
+    @Bean ServletWebServerFactory customerWebServerFactory(Environment env) {
         TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        serverFactory.setPort(9090);
+        serverFactory.setContextPath(env.getProperty("contextPath"));
         return serverFactory;
     }
 }

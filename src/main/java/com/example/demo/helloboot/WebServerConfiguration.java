a8b180d9e32ca169c332d9712918096ca1d1b311
do-park <doheepa@kakao.com>
2024-03-23 22:06:38 +0900

    feature: 프로퍼티 클래스의 분리

--- src/main/java/com/example/demo/helloboot/WebServerConfiguration.java
+++ src/main/java/com/example/demo/helloboot/WebServerConfiguration.java
@@ -1,24 +1,30 @@
 package com.example.demo.helloboot;
 
-import org.springframework.beans.factory.annotation.Value;
+import com.example.demo.config.autoconfig.ServerProperties;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration(proxyBeanMethods = false)
 public class WebServerConfiguration {
-    @Value("${contextPath:}")
-    String contextPath;
 
-    @Value("${port:8080}")
-    int port;
-
-    @Bean ServletWebServerFactory customerWebServerFactory() {
+    @Bean ServletWebServerFactory customerWebServerFactory(ServerProperties properties) {
         TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 
-        serverFactory.setContextPath(this.contextPath);
-        serverFactory.setPort(port);
+        serverFactory.setContextPath(properties.getContextPath());
+        serverFactory.setPort(properties.getPort());
         return serverFactory;
+    }
+
+    @Bean
+    public ServerProperties serverProperties(Environment env) {
+        ServerProperties properties = new ServerProperties();
+
+        properties.setContextPath(env.getProperty("contextPath"));
+        properties.setPort(Integer.parseInt(env.getProperty("port")));
+
+        return properties;
     }
 }

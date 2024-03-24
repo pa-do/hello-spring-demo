20db37afaa84c04fdc8c3930d6b94416cba6c2d4
do-park <doheepa@kakao.com>
2024-03-24 17:36:43 +0900

    feature: JdbcTempate과 트랜잭션 매니저 구성

--- src/main/java/com/example/demo/config/autoconfig/DataSourceConfig.java
+++ src/main/java/com/example/demo/config/autoconfig/DataSourceConfig.java
@@ -3,8 +3,13 @@ package com.example.demo.config.autoconfig;
 import com.example.demo.config.ConditionalMyOnClass;
 import com.example.demo.config.EnableMyConfigurationProperties;
 import com.example.demo.config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.support.JdbcTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 import java.sql.Driver;
@@ -12,6 +17,7 @@ import java.sql.Driver;
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.springframework.jdbc.core.JdbcOperations")
 @EnableMyConfigurationProperties(MyDataSourceProperties.class)
+@EnableTransactionManagement
 public class DataSourceConfig {
     @Bean DataSource dataSource(MyDataSourceProperties properties) throws ClassNotFoundException {
         SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
@@ -22,6 +28,20 @@ public class DataSourceConfig {
         dataSource.setPassword(properties.getPassword());
 
         return dataSource;
+    }
+
+    @Bean
+    @ConditionalOnSingleCandidate(DataSource.class)
+    @ConditionalOnMissingBean
+    JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    @ConditionalOnSingleCandidate(DataSource.class)
+    @ConditionalOnMissingBean
+    JdbcTransactionManager jdbcTransactionManager(DataSource dataSource) {
+        return new JdbcTransactionManager(dataSource);
     }
 
 }
--- src/test/java/com/example/demo/helloboot/DataSourceTest.java
+++ src/test/java/com/example/demo/helloboot/DataSourceTest.java
@@ -1,19 +1,13 @@
 package com.example.demo.helloboot;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = DemoApplication.class)
-@TestPropertySource("classpath:/application.properties")
+@HelloBootTest
 public class DataSourceTest {
     @Autowired DataSource dataSource;
 

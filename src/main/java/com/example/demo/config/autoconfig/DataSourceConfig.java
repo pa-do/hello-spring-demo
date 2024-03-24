3b1e5224e6ba4971a2ad70ed27268415d78995ac
do-park <doheepa@kakao.com>
2024-03-24 16:41:33 +0900

    feature: DataSource 자동 구성 클래스

---
+++ src/main/java/com/example/demo/config/autoconfig/DataSourceConfig.java
@@ -0,0 +1,27 @@
+package com.example.demo.config.autoconfig;
+
+import com.example.demo.config.ConditionalMyOnClass;
+import com.example.demo.config.EnableMyConfigurationProperties;
+import com.example.demo.config.MyAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Driver;
+
+@MyAutoConfiguration
+@ConditionalMyOnClass("org.springframework.jdbc.core.JdbcOperations")
+@EnableMyConfigurationProperties(MyDataSourceProperties.class)
+public class DataSourceConfig {
+    @Bean DataSource dataSource(MyDataSourceProperties properties) throws ClassNotFoundException {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+
+        dataSource.setDriverClass((Class<? extends Driver>) Class.forName(properties.getDriverClassName()));
+        dataSource.setUrl(properties.getUrl());
+        dataSource.setUsername(properties.getUsername());
+        dataSource.setPassword(properties.getPassword());
+
+        return dataSource;
+    }
+
+}
